01a1a490e21962c5f3c429981ce2d41ca66af576
Artem <artem.yablochnyi@yandex.ru>
2022-06-15 11:09:29 +0200

    Updated admin.product.index

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\Product;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,8 +17,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        User::create([
+            'name' => 'admin',
+            'email' =>  'admin@admin.com',
+            'role' => 1,
+            'password' => Hash::make('admin2022'),
+        ]);
 
-        Product::factory()->count(30)->create();
+//        Product::factory()->count(30)->create();
 
         // \App\Models\User::factory(10)->create();
 
