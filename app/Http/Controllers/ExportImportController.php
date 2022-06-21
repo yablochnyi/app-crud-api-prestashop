bcd907c294d7109f1a9f5a62fd6af740a896bcab
Artem <artem.yablochnyi@yandex.ru>
2022-06-21 15:09:22 +0200

    Updated export/import with category

--- app/Http/Controllers/ExportImportController.php
+++ app/Http/Controllers/ExportImportController.php
@@ -8,7 +8,6 @@ use Maatwebsite\Excel\Facades\Excel;
 
 class ExportImportController extends Controller
 {
-
     public function export()
     {
         return Excel::download(new Products(), 'products.xlsx');
--- app/Imports/Products.php
+++ app/Imports/Products.php
@@ -2,12 +2,20 @@
 
 namespace App\Imports;
 
+use App\Models\Category;
 use App\Models\Product;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
 class Products implements ToModel, WithHeadingRow
 {
+
+    private $categories;
+
+    public function __construct()
+    {
+        $this->categories = Category::select('id', 'title')->get();
+    }
     /**
     * @param array $row
     *
@@ -15,11 +23,13 @@ class Products implements ToModel, WithHeadingRow
     */
     public function model(array $row)
     {
+        $category = $this->categories->where('title', $row['category'])->first();
+
         return new Product([
             'item_code' => $row['item_code'],
             'product_number' => $row['product_number'],
             'product_name' => $row['product_name'],
-            'category_id' => $row['category'],
+            'category_id' => $category->id,
             'unit' => $row['unit'],
             'quantity' => $row['quantity'],
             'price_aed' => $row['price_aed'],
