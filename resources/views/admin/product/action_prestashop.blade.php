69f3f05bf1db448f64f7afe37ca9bcd40ffe573e
Artem <artem.yablochnyi@yandex.ru>
2022-07-01 14:52:43 +0200

    added rme sidebar and update style

--- resources/views/admin/product/action_prestashop.blade.php
+++ resources/views/admin/product/action_prestashop.blade.php
@@ -1,18 +1,24 @@
-<a href="{{ route('add.prestashop',$id) }}" data-toggle="tooltip" data-original-title="Add"
-   class="edit btn btn-success edit">
-    Add to prestashop
-</a>
+<form method="post" action="{{ route('add.prestashop',$id) }}">
+    @csrf
+    <button type="submit" class="edit btn btn-custom-success edit mt-1">
+        Add to prestashop
+    </button>
+</form>
+{{--<a href="{{ route('add.prestashop',$id) }}" data-toggle="tooltip" data-original-title="Add"--}}
+{{--   class="edit btn btn-custom-success edit">--}}
+{{--    Add to prestashop--}}
+{{--</a>--}}
 <form method="post" action="{{ route('update.price.prestashop',$id) }}">
     @csrf
     @method('put')
-    <button type="submit" class="edit btn btn-success edit mt-1">
+    <button type="submit" class="edit btn btn-custom-success edit mt-1">
             Update price
     </button>
 </form>
 <form method="post" action="{{ route('update.quantity.prestashop',$id) }}">
     @csrf
     @method('put')
-    <button type="submit" class="edit btn btn-success edit mt-1">
+    <button type="submit" class="edit btn btn-custom-success edit mt-1">
             Update quantity
     </button>
 </form>
